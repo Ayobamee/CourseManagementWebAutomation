b46ef5b7b7d16e61223e98a7da815da39aba535b
Ayobamee <ayobami.elutade@com>
2022-12-25 23:13:14 +0100

    cypress dashboard script

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  projectId: 'pcrtom',
-  reporter: 'mochawesome',
+  projectId: "w5b6zd",
+  reporter: "mochawesome",
   retries: {
     runMode: 2,
     openMode: 1,
   },
   env: {
-    username: 'ay@mail.com',
-    password: 'pass1234',
+    username: "ay@mail.com",
+    password: "pass1234",
   },
   defaultCommandTimeout: 30000,
   pageLoadTimeout: 50000,
@@ -18,9 +18,16 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://tawdry-rule.surge.sh/',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://tawdry-rule.surge.sh/",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
-})
+
+  scripts: {
+    test: "npx cypress run",
+    open: "npx cypress open",
+    cloudTest:
+      "npx cypress run --record --key 781adacf-be2f-4610-ac7d-2537a429f8b9",
+  },
+});
